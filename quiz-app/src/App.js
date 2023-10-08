f5d4eea13a090bffef6597fb2b7801f354b7d660
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-10-08 14:09:57 +0400

    Design is added to quiz-box

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 import MainQuiz from "./components/MainQuiz";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <MainQuiz />
     </div>
   );
--- quiz-app/src/components/MainQuiz.js
+++ quiz-app/src/components/MainQuiz.js
@@ -73,9 +73,11 @@ class MainQuiz extends React.Component {
     if (isEnd) {
       return (
         <div className="result">
-          <h3>Your correct answers are {this.state.score} </h3>
+          <div className="show-result">
+            Your correct answers are <b className="score">{this.state.score}</b>{" "}
+          </div>
           <div>
-            The correct answer's for the questions was
+            The correct answer's for the questions was :
             <ul>
               {quizData.map((item, index) => (
                 <li className="ui floating message options" key={index}>
@@ -88,8 +90,8 @@ class MainQuiz extends React.Component {
       );
     } else {
       return (
-        <div className="App">
-          <h1>{this.state.questions} </h1>
+        <div className="quiz-box">
+          <h4>{this.state.questions} </h4>
           <span>{`Questions ${currentQuestion}  out of ${
             quizData.length - 1
           } remaining `}</span>
@@ -105,18 +107,25 @@ class MainQuiz extends React.Component {
             </p>
           ))}
           {currentQuestion < quizData.length - 1 && (
-            <button
-              className="ui inverted button"
-              disabled={this.state.disabled}
-              onClick={this.nextQuestionHandler}
-            >
-              Next
-            </button>
+            <div className="btn-div">
+              <button
+                className="ui inverted-button"
+                disabled={this.state.disabled}
+                onClick={this.nextQuestionHandler}
+              >
+                Next
+              </button>
+            </div>
           )}
           {currentQuestion === quizData.length - 1 && (
-            <button className="ui inverted button" onClick={this.finishHandler}>
-              Finish
-            </button>
+            <div className="btn-div">
+              <button
+                className="ui inverted-button"
+                onClick={this.finishHandler}
+              >
+                Finish
+              </button>
+            </div>
           )}
         </div>
       );
